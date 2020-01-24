0127e8ac165967d6851cd0834905d624aea55e59
ArturCarlos <arturcarlossantiago@gmail.com>
2020-01-24 10:44:52 -0300

    arquivos modificados

--- index.php
+++ index.php
@@ -1,7 +1,65 @@
+<?php require_once 'config.php' ?>
 
-<?php require_once 'config.php'; ?>	<?php require_once DBAPI; ?>
-<?php 		$db = open_database(); 				if ($db) {
-    echo "<h1>Banco de Dados Conectado!</h1>";
-} else {
-    echo "<h1>ERRO: Não foi possível Conectar!</h1>";
-}	?>
+<?php include(HEADER_TEMPLATE); ?>
+
+
+<h1>Selecione uma Calculadora</h1>
+<hr/>
+
+<div class="row">
+
+    <!--Pagina Juros simples-->
+    <div class="col-xs-6 col-sm-3 col-md-2"><a href="pages/juros-simples.php" class="btn btn-warning">
+            <div class="row">
+                <div class="col-xs-12 text-center"><i class="fa fa-calculator fa-5x"></i></div>
+
+                <div class="col-xs-12 text-center"><p>Juros Simples</p></div>
+            </div>
+        </a>
+    </div>
+
+    <!--Pagina Juros Composto-->
+    <div class="col-xs-6 col-sm-3 col-md-2"><a href="pages/juros-composto.php" class="btn btn-success">
+            <div class="row">
+                <div class="col-xs-12 text-center"><i class="fa fa-calculator fa-5x"></i></div>
+
+                <div class="col-xs-12 text-center"><p>Juros Composto</p></div>
+            </div>
+        </a>
+    </div>
+
+    <!--Pagina Mosntate-->
+    <div class="col-xs-6 col-sm-3 col-md-2"><a href="pages/montante.php" class="btn btn-primary">
+            <div class="row">
+                <div class="col-xs-12 text-center"><i class="fa fa-calculator fa-5x"></i></div>
+
+                <div class="col-xs-12 text-center"><p>Montante</p></div>
+            </div>
+        </a>
+    </div>
+
+    <!--Pagina Desconto simples-->
+    <div class="col-xs-6 col-sm-3 col-md-2"><a href="pages/desconto-simples.php" class="btn btn-info">
+            <div class="row">
+                <div class="col-xs-12 text-center"><i class="fa fa-calculator fa-5x"></i></div>
+
+                <div class="col-xs-12 text-center"><p>Desconto Simples</p></div>
+            </div>
+        </a>
+    </div>
+
+    <!--Pagina Juros simples-->
+    <div class="col-xs-6 col-sm-3 col-md-2"><a href="pages/desconto-composto.php" class="btn btn-default">
+            <div class="row">
+                <div class="col-xs-12 text-center"><i class="fa fa-calculator fa-5x"></i></div>
+
+                <div class="col-xs-12 text-center"><p>Desconto Composto</p></div>
+            </div>
+        </a>
+    </div>
+
+</div>
+
+
+
+<?php include(FOOTER_TEMPLATE); ?>
